484f595288c7ac1c927cc2f02520c2c6a69933dd
moehreag <moehreag@gmail.com>
2023-12-24 12:39:50 +0100

    implement exclusion for smallships
    - this does not need a release directly as smallships isn't even updated beyond 1.20.1 which this mod doesn't support anymore.
    closes #2

--- src/main/java/io/github/moehreag/realistic_rowing/RealisticRowing.java
+++ src/main/java/io/github/moehreag/realistic_rowing/RealisticRowing.java
@@ -1,21 +1,50 @@
 package io.github.moehreag.realistic_rowing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.github.moehreag.realistic_rowing.mixin.BoatEntityAccessor;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.vehicle.BoatEntity;
 
 public class RealisticRowing implements ClientModInitializer {
+	private static final Map<Class<?>, Boolean> boats = new HashMap<>();
+	private static final Map<Entity, Boolean> activationCache = new HashMap<>();
 	@Override
 	public void onInitializeClient() {
+		try {
+			if (FabricLoader.getInstance().isModLoaded("smallships")) {
+				boats.put(Class.forName("com.talhanation.smallships.world.entity.ship.Ship"), false);
+			}
+		} catch (ClassNotFoundException ignored) {
 
+		}
+	}
+
+	public static boolean shouldActivate(Entity e){
+		if (activationCache.containsKey(e)){
+			return activationCache.get(e);
+		}
+		boolean value = true;
+		for (Map.Entry<Class<?>, Boolean> entry : boats.entrySet()){
+			if (entry.getKey().isAssignableFrom(e.getClass())){
+				value = entry.getValue();
+			}
+		}
+		activationCache.put(e, value);
+		return value;
 	}
 
 	public static void setPaddleOffset(BoatEntity entity, ModelPart part){
-		if(entity.getPassengerList().size() == ((BoatEntityAccessor)entity).getMaxPassengerCount()){
-			part.pivotX = 7;
-		} else {
-			part.pivotX = 3;
+		if (shouldActivate(entity)) {
+			if (entity.getPassengerList().size() == ((BoatEntityAccessor) entity).getMaxPassengerCount()) {
+				part.pivotX = 7;
+			} else {
+				part.pivotX = 3;
+			}
 		}
 	}
 }
--- src/main/java/io/github/moehreag/realistic_rowing/mixin/BoatEntityMixin.java
+++ src/main/java/io/github/moehreag/realistic_rowing/mixin/BoatEntityMixin.java
@@ -2,6 +2,7 @@ package io.github.moehreag.realistic_rowing.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import io.github.moehreag.realistic_rowing.RealisticRowing;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.BoatEntity;
@@ -23,25 +24,35 @@ public abstract class BoatEntityMixin extends Entity {
 
 	@WrapOperation(method = "getPassengerAttachmentPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;getPassengerHorizontalOffset()F"))
 	private float realisticrowing$movePassenger(BoatEntity instance, Operation<Float> original){
-		return original.call(instance) + 0.6F + (!(instance instanceof ChestBoatEntity) ? 0.15F : 0);
+		if (RealisticRowing.shouldActivate(instance)) {
+			return original.call(instance) + 0.6F + (!(instance instanceof ChestBoatEntity) ? 0.15F : 0);
+		}
+		return original.call(instance);
 	}
 
 	@Inject(method = "clampPassengerYaw", at = @At("HEAD"))
 	private void realisiticRowing$turnPlayer(Entity passenger, CallbackInfo ci){
-		if (MathHelper.angleBetween(passenger.getYaw(), this.getYaw()) < 5) {
-			passenger.setYaw(getYaw() + 180);
+		if (RealisticRowing.shouldActivate(this)) {
+			if (MathHelper.angleBetween(passenger.getYaw(), this.getYaw()) < 5) {
+				passenger.setYaw(getYaw() + 180);
+			}
 		}
 	}
 
 	@ModifyArg(method = "clampPassengerYaw", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setBodyYaw(F)V", ordinal = 0))
 	private float realisticRowing$clampPassengerYaw$setBodyYaw(float bodyYaw){
-
-		return bodyYaw + 180;
+		if (RealisticRowing.shouldActivate(this)) {
+			return bodyYaw + 180;
+		}
+		return bodyYaw;
 	}
 
 	@ModifyArg(method = "clampPassengerYaw", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;wrapDegrees(F)F"))
 	private float realisticRowing$clampPassengerYaw$wrapDegrees(float degrees){
-		return degrees - 180;
+		if (RealisticRowing.shouldActivate(this)) {
+			return degrees - 180;
+		}
+		return degrees;
 	}
 
 }
